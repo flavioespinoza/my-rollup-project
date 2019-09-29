3737c65ea3ea3937bf40dda04b743ed21f269a81
flavioespinoza <flavio.espinoza@gmail.com>
2019-09-28 22:14:32 -0600

    stuff

--- node_modules/datedash/methods/importModules.js
+++
@@ -1,8 +0,0 @@
-import { addDays, subtractDays } from 'datedash'
-
-module.exports = () => {
-
-	// 'importModules.js returns nothing.  
-	// It is required for the npm run doc script to generate the docs side nav
-
-}
